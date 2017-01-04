b9e746c5356fa704ab706affa1891196b0a1e996
Tyson Wolker <tysonwolker@gmail.com>
2017-01-04 17:57:20 +1100

    Removed blank echo commands for reals

--- zsh-tab-colors.plugin.zsh
+++ zsh-tab-colors.plugin.zsh
@@ -1,20 +1,18 @@
 function get_random_rgb_from_cwd () {
   local color=$1;
-  echo $(python -c "import random; import os; random.seed('$color' + os.getcwd()); print random.randint(0,255)")
+  printf $(python -c "import random; import os; random.seed('$color' + os.getcwd()); print random.randint(0,255)")
 }
 
 function set_iterm_tab_color () {
   local color=$1; 
   local color_rgb_value=$(get_random_rgb_from_cwd $color)
-  echo "\033]6;1;bg;$color;brightness;$color_rgb_value\a"
+  printf "\033]6;1;bg;$color;brightness;$color_rgb_value\a"
 }
 
 function automatic_iterm_tab_color_cwd () {
-  {
-    set_iterm_tab_color "red"
-    set_iterm_tab_color "green"
-    set_iterm_tab_color "blue"
-  } &> /dev/null
+  set_iterm_tab_color "red" 
+  set_iterm_tab_color "green"
+  set_iterm_tab_color "blue"
 }
 
 automatic_iterm_tab_color_cwd 
