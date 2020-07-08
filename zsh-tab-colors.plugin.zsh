67f783992464e993ec80776b02a1d4791d52bea6
Tim Morgan <tim@timmorgan.org>
2020-07-08 10:21:54 -0500

    Speed up color selection by reducing calls to Python
    This is admittedly not nearly as pretty as Tyson's implementation, but
    it is quite a bit faster!
    
    Based on my benchmarks, time measurements are:
    
    - Before: average 145 milliseconds per run
    - After: average 50 milliseconds per run
    
    I (personally) willing to trade code style for speed. :^)

--- zsh-tab-colors.plugin.zsh
+++ zsh-tab-colors.plugin.zsh
@@ -1,21 +1,10 @@
-function get_random_rgb_from_cwd () {
-  local color=$1;
-  printf $(python -c "import random; import os; random.seed('$color' + os.getcwd()); print((random.randint(0,255)+255)/2)")
-}
-
-function set_iterm_tab_color () {
-  local color=$1; 
-  local color_rgb_value=$(get_random_rgb_from_cwd $color)
-  printf "\033]6;1;bg;$color;brightness;$color_rgb_value\a"
-}
-
 function automatic_iterm_tab_color_cwd () {
-  set_iterm_tab_color "red" 
-  set_iterm_tab_color "green"
-  set_iterm_tab_color "blue"
+  python -c "import random; import os; \
+             random.seed('red'   + os.getcwd()); print((\"\\033]6;1;bg;red;brightness;\"   + str((random.randint(0,255)+255)/2)) + \"\\a\"); \
+             random.seed('green' + os.getcwd()); print((\"\\033]6;1;bg;green;brightness;\" + str((random.randint(0,255)+255)/2)) + \"\\a\"); \
+             random.seed('blue'  + os.getcwd()); print((\"\\033]6;1;bg;blue;brightness;\"  + str((random.randint(0,255)+255)/2)) + \"\\a\");"
 }
 
 automatic_iterm_tab_color_cwd 
 autoload -U add-zsh-hook
 add-zsh-hook chpwd automatic_iterm_tab_color_cwd 
-
