870dafbf89908c04e64190d150b68f891959a218
Tim Morgan <tim@timmorgan.org>
2020-07-13 17:45:49 -0500

    Eliminate extra blank lines when changing directories

--- zsh-tab-colors.plugin.zsh
+++ zsh-tab-colors.plugin.zsh
@@ -1,8 +1,9 @@
 function automatic_iterm_tab_color_cwd () {
   python -c "import random; import os; \
-             random.seed('red'   + os.getcwd()); print((\"\\033]6;1;bg;red;brightness;\"   + str((random.randint(0,255)+255)/2)) + \"\\a\"); \
-             random.seed('green' + os.getcwd()); print((\"\\033]6;1;bg;green;brightness;\" + str((random.randint(0,255)+255)/2)) + \"\\a\"); \
-             random.seed('blue'  + os.getcwd()); print((\"\\033]6;1;bg;blue;brightness;\"  + str((random.randint(0,255)+255)/2)) + \"\\a\");"
+             random.seed('red'   + os.getcwd()); print((\"\\033]6;1;bg;red;brightness;\"   + str((random.randint(0,255)+255)/2)) + \"\\a\"),; \
+             random.seed('green' + os.getcwd()); print((\"\\033]6;1;bg;green;brightness;\" + str((random.randint(0,255)+255)/2)) + \"\\a\"),; \
+             random.seed('blue'  + os.getcwd()); print((\"\\033]6;1;bg;blue;brightness;\"  + str((random.randint(0,255)+255)/2)) + \"\\a\"),; \
+             print(\"\\r\"),"
 }
 
 automatic_iterm_tab_color_cwd 
