8509cb3b12f82e58cc5108669942116d19ebeda5
Ashton Kinslow <github@ashtonkinslow.com>
2023-02-21 14:35:31 -0600

    update plugin to use py3 if available

--- zsh-tab-colors.plugin.zsh
+++ zsh-tab-colors.plugin.zsh
@@ -1,10 +1,16 @@
+pycmd="python"
+
+if command -v python3 &>/dev/null; then
+  pycmd="python3"
+fi
+
 function automatic_iterm_tab_color_cwd () {
-  python -c "import random; import os; \
+  $pycmd -c "import random; import os; \
              random.seed('red'   + os.getcwd()); print((\"\\033]6;1;bg;red;brightness;\"   + str((random.randint(0,255)+255)/2)) + \"\\a\"); \
              random.seed('green' + os.getcwd()); print((\"\\033]6;1;bg;green;brightness;\" + str((random.randint(0,255)+255)/2)) + \"\\a\"); \
              random.seed('blue'  + os.getcwd()); print((\"\\033]6;1;bg;blue;brightness;\"  + str((random.randint(0,255)+255)/2)) + \"\\a\");"
 }
 
-automatic_iterm_tab_color_cwd 
+automatic_iterm_tab_color_cwd
 autoload -U add-zsh-hook
-add-zsh-hook chpwd automatic_iterm_tab_color_cwd 
+add-zsh-hook chpwd automatic_iterm_tab_color_cwd
